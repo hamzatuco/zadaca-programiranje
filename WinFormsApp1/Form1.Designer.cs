da9ed462c91460c5b75328a8d86fbf117dfb9c22
Hamza <htuco04@gmail.com>
2023-02-22 23:51:08 +0100

    zadaca

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kvadrat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Pravouganik";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 125);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 50);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Krug";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 181);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 50);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Elipsa";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(191, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(416, 202);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 237);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(122, 42);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Linija";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(329, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Prostor za crtanje ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(345, 472);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Ucitaj";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(279, 281);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(208, 149);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(347, 437);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Ucitana slika";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(532, 254);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Sacuvaj";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 507);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,265 @@
+﻿
+using System.Drawing.Imaging;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+
+
+
+    {
+        private Point _startPoint;
+        private Point _endPoint;
+        private bool _isDrawing;
+        private Pen _pen;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _pen = new Pen(Color.Black, 2);
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new ColorDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var fillColor = dialog.Color;
+
+                    using (var dialog2 = new ColorDialog())
+                    {
+                        if (dialog2.ShowDialog() == DialogResult.OK)
+                        {
+                            var borderColor = dialog2.Color;
+                            DrawSquare(fillColor, borderColor);
+                        }
+                    }
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new ColorDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var fillColor = dialog.Color;
+
+                    using (var dialog2 = new ColorDialog())
+                    {
+                        if (dialog2.ShowDialog() == DialogResult.OK)
+                        {
+                            var borderColor = dialog2.Color;
+                            DrawRectangle(fillColor, borderColor);
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        private void DrawSquare(Color fillColor, Color borderColor)
+        {
+            Graphics graphics = panel1.CreateGraphics();
+            Pen pen = new Pen(borderColor);
+            SolidBrush brush = new SolidBrush(fillColor);
+            Rectangle rectangle = new Rectangle(50, 50, 100, 100);
+            graphics.FillRectangle(brush, rectangle);
+            graphics.DrawRectangle(pen, rectangle);
+        }
+
+        private void DrawRectangle(Color fillColor, Color borderColor)
+        {
+            Graphics graphics = panel1.CreateGraphics();
+            Pen pen = new Pen(borderColor);
+            SolidBrush brush = new SolidBrush(fillColor);
+            Rectangle rectangle = new Rectangle(50, 50, 150, 100);
+            graphics.FillRectangle(brush, rectangle);
+            graphics.DrawRectangle(pen, rectangle);
+        }
+        private void DrawCircle(Color fillColor, Color borderColor)
+        {
+            Graphics graphics = panel1.CreateGraphics();
+            Pen pen = new Pen(borderColor);
+            SolidBrush brush = new SolidBrush(fillColor);
+            Rectangle rectangle = new Rectangle(50, 50, 100, 100);
+            graphics.FillEllipse(brush, rectangle);
+            graphics.DrawEllipse(pen, rectangle);
+        }
+
+        private void DrawLine(Color lineColor)
+        {
+            Graphics graphics = panel1.CreateGraphics();
+            Pen pen = new Pen(lineColor);
+            graphics.DrawLine(pen, new Point(50, 50), new Point(150, 150));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new ColorDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var fillColor = dialog.Color;
+
+                    using (var dialog2 = new ColorDialog())
+                    {
+                        if (dialog2.ShowDialog() == DialogResult.OK)
+                        {
+                            var borderColor = dialog2.Color;
+                            DrawCircle(fillColor, borderColor);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void DrawEllipse(Color fillColor, Color borderColor)
+        {
+            Graphics graphics = panel1.CreateGraphics();
+            Pen pen = new Pen(borderColor);
+            graphics.FillEllipse(new SolidBrush(fillColor), new Rectangle(50, 50, 100, 150));
+            graphics.DrawEllipse(pen, new Rectangle(50, 50, 100, 150));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new ColorDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var fillColor = dialog.Color;
+
+                    using (var dialog2 = new ColorDialog())
+                    {
+                        if (dialog2.ShowDialog() == DialogResult.OK)
+                        {
+                            var borderColor = dialog2.Color;
+                            DrawEllipse(fillColor, borderColor);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _startPoint = e.Location;
+            _isDrawing = true;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isDrawing)
+            {
+                _endPoint = e.Location;
+                panel1.Invalidate();
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _isDrawing = false;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (_isDrawing)
+            {
+                e.Graphics.DrawLine(_pen, _startPoint, _endPoint);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new ColorDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var penColor = dialog.Color;
+                    _pen.Color = penColor;
+                }
+            }
+        }
+
+        private void button7_CLick(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PNG files (*.png)|*.png";
+            saveFileDialog.FileName = "MyDrawing.png";
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap bitmap = new Bitmap(panel1.Width, panel1.Height);
+                panel1.DrawToBitmap(bitmap, new Rectangle(Point.Empty, panel1.Size));
+                bitmap.Save(saveFileDialog.FileName, ImageFormat.Png);
+            }
+        }
+
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            // kreiramo novi OpenFileDialog objekat
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // postavljamo opcije za dijalog
+            openFileDialog.Filter = "PNG files (*.png)|*.png|All files (*.*)|*.*";
+            openFileDialog.RestoreDirectory = true;
+
+            // prikazujemo dijalog i proveravamo da li je korisnik izabrao sliku
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // učitavamo sliku u PictureBox kontrolu
+                pictureBox1.Image = Image.FromFile(openFileDialog.FileName);
+            }
+        }
+
+       
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+          
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "JPEG files (*.jpg)|*.jpg";
+                saveFileDialog.FileName = "MyDrawing.jpg";
+                saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap bitmap = new Bitmap(panel1.Width * 2, panel1.Height * 2);
+
+                    using (Graphics graphics = Graphics.FromImage(bitmap))
+                    {
+                        graphics.Clear(Color.White);
+                        graphics.ScaleTransform(2f, 2f);
+                        panel1.DrawToBitmap(bitmap, panel1.Bounds);
+                    }
+
+                    bitmap.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
+                }
+            
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
